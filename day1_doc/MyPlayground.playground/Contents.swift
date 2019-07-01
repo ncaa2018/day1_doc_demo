6a8bc9238839548ac22de89d40573ced53658bb1
Naomi Amuzie <ncaa2018@mymail.pomona.edu>
2019-07-01 11:54:41 -0700

    removes and returns the second element

--- day1_doc/MyPlayground.playground/Contents.swift
+++ day1_doc/MyPlayground.playground/Contents.swift
@@ -3,3 +3,5 @@ import UIKit
 var str = "Hello, playground"
 var arr = [1, 3, 5, 7]
 // remove and print the 2nd element
+let removed = arr.remove(at: 1)
+print(removed) // if I print the array again here, it would have the element remove
